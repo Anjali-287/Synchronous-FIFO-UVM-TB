11438c6b6b7cbd8b8434c4b58630b80b68fff65c
Umaru28 <58917448+Umaru28@users.noreply.github.com>
2020-07-07 23:10:22 +0530

    Create fifo_sequence.sv

---
+++ fifo_sequence.sv
@@ -0,0 +1,45 @@
+
+
+//Fifo Sequence
+
+class fifo_write_sequence extends uvm_sequence#(fifo_seq_item);
+  `uvm_object_utils(fifo_write_sequence)
+  
+  //---------------------------------------
+  //Constructor
+  //---------------------------------------
+  function new(string name="fifo_write_sequence");
+    super.new(name);
+  endfunction
+  bit full;
+  
+  virtual task body();
+    fifo_seq_item seq;
+    fifo_seq_item rsp;
+    //---------------------------------------
+    //Write sequence
+    //---------------------------------------
+    do begin
+      seq=new();
+      start_item(seq);
+      assert(seq.randomize()with{seq.wr==1;});
+      finish_item(seq);
+      get_response(rsp);
+      $display("((((((((%0d)))))))",rsp.full);
+    end while(rsp.full!=1);
+    //---------------------------------------
+    //Read sequence
+    //---------------------------------------
+    do begin
+      seq=new();
+      start_item(seq);
+      assert(seq.randomize()with{seq.rd==1;});
+      finish_item(seq);
+      get_response(rsp);
+      $display("((((((((%0d)))))))",rsp.empty);
+    end while(rsp.empty!=1);
+    
+  endtask
+  
+
+endclass
