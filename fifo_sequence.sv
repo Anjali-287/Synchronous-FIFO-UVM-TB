bdb971c979c636f9211ab6d99aec9a6e3065e195
Anjali Jaiswal <58917448+Anjali-287@users.noreply.github.com>
2020-08-27 19:51:34 +0530

    Update fifo_sequence.sv

--- fifo_sequence.sv
+++ fifo_sequence.sv
@@ -1,43 +1,46 @@
-
-
-//Fifo Sequence
-
-class fifo_write_sequence extends uvm_sequence#(fifo_seq_item);
-  `uvm_object_utils(fifo_write_sequence)
+class fifo_seq_item extends uvm_sequence_item;
+  
+  //---------------------------------------
+  //data and control fields
+  //---------------------------------------
+  rand bit [7:0]data_in;
+  rand bit rd;
+  rand bit wr;
+  bit full;
+  bit empty;
+  bit [7:0]data_out;
+  
+  //---------------------------------------
+  //Utility and Field macros
+  //---------------------------------------
+  `uvm_object_utils_begin(fifo_seq_item)
+  `uvm_field_int(data_in, UVM_ALL_ON)
+  `uvm_field_int(rd, UVM_ALL_ON)
+  `uvm_field_int(wr, UVM_ALL_ON)
+  `uvm_field_int(full, UVM_ALL_ON)
+  `uvm_field_int(empty, UVM_ALL_ON)
+  `uvm_field_int(data_out, UVM_ALL_ON)
+  `uvm_object_utils_end
+  
+  constraint c1{rd!=wr;};
+  
+  //---------------------------------------
+  //Pre randomize function
+  //---------------------------------------
+  function void pre_randomize();
+    if(rd)
+      data_in.rand_mode(0);
+  endfunction
   
   //---------------------------------------
   //Constructor
   //---------------------------------------
-  function new(string name="fifo_write_sequence");
+  function new(string name="fifo_seq_item");
     super.new(name);
   endfunction
-  bit full;
   
-  virtual task body();
-    fifo_seq_item seq;
-    fifo_seq_item rsp;
-    //---------------------------------------
-    //Write sequence
-    //---------------------------------------
-    do begin
-      seq=new();
-      start_item(seq);
-      assert(seq.randomize()with{seq.wr==1;});
-      finish_item(seq);
-      get_response(rsp);
-    end while(rsp.full!=1);
-    //---------------------------------------
-    //Read sequence
-    //---------------------------------------
-    do begin
-      seq=new();
-      start_item(seq);
-      assert(seq.randomize()with{seq.rd==1;});
-      finish_item(seq);
-      get_response(rsp);
-    end while(rsp.empty!=1);
-    
-  endtask
+  function string convert2string();
+    return $psprintf("data_in=%0h,data_out=%0h,wr=%0d,rd=%0d,full=%od,empty=%0d",data_in,data_out,wr,rd,full,empty);
+  endfunction
   
-
 endclass
